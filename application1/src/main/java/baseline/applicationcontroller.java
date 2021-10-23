a86a3e4d000ebffc53037c8cc04a4a01046577bc
Roesn <Bubwub04@icloud.com>
2021-10-23 15:07:59 -0400

    .xmfl basics and doc setup

---
+++ application1/src/main/java/baseline/applicationcontroller.java
@@ -0,0 +1,10 @@
+/*
+ *  UCF COP3330 Summer 2021 Application Assignment 1 Solution
+ *  Copyright 2021 first_name last_name
+ */
+
+
+package baseline;
+
+public class applicationcontroller implements Initializable {
+}
